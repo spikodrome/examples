6313277caddf111a02d43e6b1d447a335679bb15
Jeremy Magland <jmagland@flatironinstitute.org>
2019-11-21 07:41:53 -0500

    dockerized mountainsort4

---
+++ sorters/mountainsort4/test1_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SHA1_CACHE_DIR="${SHA1_CACHE_DIR:-/tmp/sha1-cache}"
+OUTPUT_DIR=$PWD/test1_docker_output
+mkdir -p $OUTPUT_DIR
+docker run -it \
+    -v /etc/passwd:/etc/passwd -u `id -u`:`id -g` \
+    -v ~/.kachery:$HOME/.kachery \
+    -v $SHA1_CACHE_DIR:/tmp/sha1-cache \
+    -v $OUTPUT_DIR:/output \
+    -e TEMPDIR=/tmp \
+    magland/spikodrome-mountainsort4 \
+    sha1dir://3ea5c9bd992de2d27402b2e83259c679d76e9319.synth_mearec_tetrode/datasets_noise10_K10_C4/001_synth
